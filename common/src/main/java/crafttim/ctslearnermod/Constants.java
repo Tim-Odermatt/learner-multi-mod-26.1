171232206b50b4cc0a5e54d4ee766e568250687d
Tim-Odermatt <91256797+Tim-Odermatt@users.noreply.github.com>
2026-04-28 20:11:26 +0200

    Initial commit

---
+++ common/src/main/java/crafttim/ctslearnermod/Constants.java
@@ -0,0 +1,10 @@
+package crafttim.ctslearnermod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "ctslearnermod"; //same as the id in gradle.properties
+	public static final String MOD_NAME = "Crafttim Learner Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
